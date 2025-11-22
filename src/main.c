0f46e2fd80a2da7f3b7afc82f7360a0802fe131e
t81dev <copyleft7212@gmail.com>
2025-11-21 20:05:55 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "t81_core.h"
+
+int main(void) {
+    T81BigInt n;
+    if (t81_bigint_from_int64(&n, 12345) != T81_OK) {
+        fprintf(stderr, "failed to init bigint\n");
+        return 1;
+    }
+    printf("Initialized T81BigInt from 12345 (len=%zu, sign=%d)\n",
+           n.len, n.sign);
+    t81_bigint_free(&n);
+    return 0;
+}
 
