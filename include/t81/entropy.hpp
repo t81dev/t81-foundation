080ed1bd8d0a090b8cc417afc8e3cd9c32804ea5
t81dev <copyleft7212@gmail.com>
2025-11-22 23:25:33 -0500

    Update entropy.hpp

--- include/t81/entropy.hpp
+++ include/t81/entropy.hpp
@@ -1,1 +1,16 @@
+#pragma once
+#include <cmath>
+
+namespace t81 {
+inline float ternary_entropy(const int* values, int len) {
+  if (len <= 0) return 0.0f;
+  int c[3] = {0,0,0};
+  for (int i=0;i<len;++i) { if (values[i] < 0) ++c[0]; else if (values[i] == 0) ++c[1]; else ++c[2]; }
+  auto H = [](float p){ return p > 0.f ? -p*std::log2(p) : 0.f; };
+  float p0 = c[0]/static_cast<float>(len);
+  float p1 = c[1]/static_cast<float>(len);
+  float p2 = c[2]/static_cast<float>(len);
+  return H(p0)+H(p1)+H(p2);
+}
+} // namespace t81
 
