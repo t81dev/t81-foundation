c8b5622c24156963606dcb9ab68836af6364de33
t81dev <copyleft7212@gmail.com>
2025-11-23 04:09:30 -0500

    Update ops.hpp

--- include/t81/tensor/ops.hpp
+++ include/t81/tensor/ops.hpp
@@ -1,4 +1,5 @@
 #pragma once
+// Aggregates common tensor ops into a single include.
 #include "t81/tensor.hpp"
 #include "t81/tensor/transpose.hpp"
 #include "t81/tensor/slice.hpp"
@@ -6,5 +7,5 @@
 #include "t81/tensor/matmul.hpp"
 #include "t81/tensor/reduce.hpp"
 #include "t81/tensor/broadcast.hpp"
-#include "t81/tensor/elementwise.hpp" // NEW
-// Aggregates common tensor ops into a single include.
+#include "t81/tensor/elementwise.hpp"
+#include "t81/tensor/unary.hpp"      // NEW
