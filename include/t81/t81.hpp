8e9ec223c7426ffc1b3b792ca068220966b5cc23
t81dev <copyleft7212@gmail.com>
2025-11-22 23:26:06 -0500

    Update t81.hpp

--- include/t81/t81.hpp
+++ include/t81/t81.hpp
@@ -1,1 +1,13 @@
+#pragma once
+#include "t81/ternary.hpp"
+#include "t81/bigint.hpp"
+#include "t81/tensor.hpp"
+#include "t81/canonfs.hpp"
+#include "t81/entropy.hpp"
+
+namespace t81 {
+// Surface aliases for T81Lang parity
+using T81BigInt = T243BigInt;
+using T81Tensor = T729Tensor;
+}
 
