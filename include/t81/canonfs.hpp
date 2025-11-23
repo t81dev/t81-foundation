64d733e291249dc6f2e2aa677e76d3d694122f71
t81dev <copyleft7212@gmail.com>
2025-11-22 23:25:07 -0500

    Update canonfs.hpp

--- include/t81/canonfs.hpp
+++ include/t81/canonfs.hpp
@@ -1,1 +1,21 @@
+#pragma once
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+namespace t81 {
+
+struct CanonHash81 { std::array<char,81> text{}; };
+
+struct CanonRef {
+  CanonHash81 target;
+  uint16_t permissions{0};
+  uint64_t expires_at{0};
+};
+
+inline CanonHash81 canonhash81_of_bytes(const void* data, size_t len) {
+  (void)data; (void)len; CanonHash81 h{}; return h; // platform seam
+}
+
+} // namespace t81
 
