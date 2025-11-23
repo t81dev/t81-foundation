bd79d784f210a9287271e2c07daa09fe22ebf635
t81dev <copyleft7212@gmail.com>
2025-11-22 23:56:05 -0500

    Create ops.hpp

---
+++ include/t81/tensor/ops.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include "t81/tensor.hpp"
+#include "t81/tensor/transpose.hpp"
+#include "t81/tensor/slice.hpp"
+#include "t81/tensor/reshape.hpp"
+
+// This header intentionally provides no new symbols;
+// it aggregates common tensor ops into a single include.
